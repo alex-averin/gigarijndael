907be60ddf4c5dc21d53f3af99fb5697bbe25f7d
Aleksei Averin <a.averin@bestdoctor.ru>
2023-05-27 17:27:37 +0400

    Basic operations on Galois fields

---
+++ gigarijndael/finite_fields/field.py
@@ -0,0 +1,65 @@
+import functools
+import operator
+
+
+class FiniteField:
+    general_polynomials = {
+        3: 0b1011,
+        4: 0b10011,
+        5: 0b100101,
+        7: 0b10011101,
+        8: 0b100011011,
+        32: 0b100000000000000000000000010001101,
+    }
+
+    def __init__(self, n: int, general_polynomial: int | None = None):
+        self.p = 2
+        self.n = n
+        self.q = self.p**n
+        self.general_polynomial = general_polynomial or self._get_general_polynomial(n)
+
+    def add(self, *polynomials: int) -> int:
+        return functools.reduce(operator.xor, polynomials)
+
+    def subtract(self, *polynomials: int) -> int:
+        return self.add(*polynomials)
+
+    def multiply(self, first: int, second: int) -> int:
+        product = 0
+        while first and second:
+            if second & 1:
+                product = self.add(product, first)
+            if first & (self.q >> 1):
+                first = self.add(first << 1, self.general_polynomial)
+            else:
+                first <<= 1
+            second >>= 1
+        return product
+
+    def divide(self, dividend: int, divisor: int) -> int:
+        inverse_divisor = self.inverse(divisor)
+        return self.multiply(dividend, inverse_divisor)
+
+    def inverse(self, polynomial: int) -> int:
+        if polynomial == 0:
+            raise ZeroDivisionError()
+        _, inverse, _ = self.egcd(polynomial, self.general_polynomial)
+        return inverse
+
+    def divmod(self, dividend: int, divisor: int) -> tuple[int, int]:
+        floor = 0
+        divisor_length = divisor.bit_length()
+        while (shift := dividend.bit_length() - divisor_length) >= 0:
+            floor = self.add(floor, 1 << shift)
+            dividend = self.add(dividend, divisor << shift)
+        return floor, dividend
+
+    def egcd(self, first: int, second: int) -> tuple[int, int, int]:
+        if first == 0:
+            return second, 0, 1
+        floor, mod = self.divmod(second, first)
+        gcd, x, y = self.egcd(mod, first)
+        return gcd, self.subtract(y, self.multiply(floor, x)), x
+
+    def _get_general_polynomial(self, n: int) -> int:
+        return self.general_polynomials[n]
---
+++ tests/test_finite_fields/test_field.py
@@ -0,0 +1,83 @@
+import pytest
+
+from gigarijndael.finite_fields.field import FiniteField
+
+
+@pytest.mark.parametrize(
+    ("first_multiplier", "second_multiplier", "expected_product", "field_n"),
+    [
+        # GF(2^3)
+        (5, 7, 6, 3),
+        # GF(2^4)
+        (9, 5, 11, 4),
+        (13, 9, 15, 4),
+        # GF(2^5)
+        (0, 0, 0, 5),
+        (1, 0, 0, 5),
+        (2, 1, 2, 5),
+        # GF(2^7)
+        (6, 21, 126, 7),
+        # GF(2^8)
+        (83, 202, 1, 8),
+        (87, 19, 254, 8),
+        (150, 200, 9, 8),
+        (212, 2, 179, 8),
+    ],
+)
+def test_field_add(first_multiplier: int, second_multiplier: int, expected_product: int, field_n: int):
+    field = FiniteField(n=field_n)
+
+    product = field.multiply(first_multiplier, second_multiplier)
+
+    assert product == expected_product
+
+
+@pytest.mark.parametrize(
+    ("dividend", "divisor", "expected_quotient"),
+    [
+        (3, 283, (0, 3)),
+        (283, 3, (246, 1)),
+        (10, 3, (6, 0)),
+        (100, 33, (3, 7)),
+    ],
+)
+def test_field_divmod(dividend: int, divisor: int, expected_quotient):
+    field = FiniteField(n=3)
+
+    quotient = field.divmod(dividend, divisor)
+
+    assert quotient == expected_quotient
+
+
+@pytest.mark.parametrize(
+    ("polynomial_a", "polynomial_b", "expected_gcd"),
+    [
+        (3, 283, (1, 246, 1)),
+    ],
+)
+def test_field_egcd(polynomial_a: int, polynomial_b: int, expected_gcd: int):
+    field = FiniteField(n=3)
+
+    gcd = field.egcd(polynomial_a, polynomial_b)
+
+    assert gcd == expected_gcd
+
+
+@pytest.mark.parametrize(
+    ("polynomial", "expected_inverse"),
+    [
+        (1, 1),
+        (3, 246),
+        (246, 3),
+        (100, 73),
+        (73, 100),
+        (255, 28),
+        (28, 255),
+    ],
+)
+def test_field_inverse(polynomial: int, expected_inverse: int):
+    field = FiniteField(n=8)
+
+    inverse = field.inverse(polynomial)
+
+    assert inverse == expected_inverse
